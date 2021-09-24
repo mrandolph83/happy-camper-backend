d99851daea768a0186dfece56d4bf67337b39f5f
mrandolph83 <mrandolph83@gmail.com>
2021-09-23 22:41:37 -0400

    Set up mock login helper methods current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+   
+        def current_user
+            # mocked version of logging in
+        User.first
+        end 
+
+        def logged_in?
+            !!current_user 
+        end
+    
 end
