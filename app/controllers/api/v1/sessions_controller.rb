fbad0e4f91456ce08a5c0139a2a53d33cb5a80ed
mrandolph83 <mrandolph83@gmail.com>
2021-09-24 16:23:36 -0400

    Added User.find_by functions and authenticate functions to set/authenticate current_user on backend

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create 
+       @user = User.find_by(username: params[:session][:username])
+
+       if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        render json: @user
+        
+       else
+        render json: {
+            error: "Invalid Username and/or Password"
+        }
 
     end
+end
 
 end
