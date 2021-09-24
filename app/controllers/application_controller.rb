69da3ddb8d5116369475c1f7bfa1b1ae9b2ba501
mrandolph83 <mrandolph83@gmail.com>
2021-09-24 16:24:34 -0400

    Updated current_user authentication to authenticate the user via the user logging into the current session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::API
    
         def current_user
-            # mocked version of logging in
-        User.first
+            User.find(session[:user_id])
         end 
 
         def logged_in?
