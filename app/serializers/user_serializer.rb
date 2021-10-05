bb9a9878dc9447fbb5b846a73b89168cde6ecd94
mrandolph83 <mrandolph83@gmail.com>
2021-10-05 11:35:35 -0400

    Fixed syntax error on user serializer, the  has_many reviews setup

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,6 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :username, :user_favorites_id
 
-  has many :reviews, serializer: UserSerializer
+  has_many :reviews, serializer: ReviewSerializer
 end
+
