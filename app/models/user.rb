c4f698b9a75f6d6f37cb31ff7467b55d96120663
mrandolph83 <mrandolph83@gmail.com>
2021-10-11 14:37:23 -0400

    Add validations to :name and :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :reviews
-    has_many :user_favorites
+    
     has_many :rec_areas, through: :user_favorites
 
     validates :name, :username, presence: true  
+    validates :username, uniqueness: true
 
 end
