d8ff1bf2e0fd889fdf6add8f1197031c24810804
mrandolph83 <mrandolph83@gmail.com>
2021-10-14 21:28:42 -0400

    get_current_user and logged_in? helper methods functional

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,13 @@ class Api::V1::SessionsController < ApplicationController
 
     def create 
        @user = User.find_by(username: params[:session][:username])
+      
         
        if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id
+        
         render json: UserSerializer.new(@user), status: :ok
+
        else
         render json: {
             error: "Invalid Username and/or Password"
@@ -14,17 +17,21 @@ class Api::V1::SessionsController < ApplicationController
     end
 
     def get_current_user
-        
+        @user = User.find_by(username: params[:session][:username])
         if logged_in?
-            render json: current_user
-            # render json: UserSerializer.new(current_user)
+            
+
+            # render json: current_user
+            render json: UserSerializer.new(current_user)
             # current_user = User.find(cookies[:current_user_id])
             
             
            
         else
+            
             render json: {
-                error: "Get Current User"
+                
+                error: "Nobody is Logged In"
             }
         end
     end 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::API
  include ::ActionController::Cookies  
         def current_user      
-            User.find_by(id: session[:user_id])
+          
+            User.find_by(session[:user_id])
+            
+            
         end 
 
         def logged_in?
