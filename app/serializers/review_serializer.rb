b9aca8749cce21e59ca9ee2f11a39d13b5f21d53
mrandolph83 <mrandolph83@gmail.com>
2021-10-15 01:42:14 -0400

    Add relationship of revieww belongs_to rec_area

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -3,4 +3,6 @@ class ReviewSerializer
 
   
   attributes :description, :picture, :date, :user_id, :rec_area_id, :nature_review, :amenities_review, :activities_review, :family_review
-end
+
+  belongs_to :rec_area, serializer: RecAreaSerializer
+end 
