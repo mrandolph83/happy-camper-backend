af4f7ed465a5aae715700a3430703074d653a510
mrandolph83 <mrandolph83@gmail.com>
2021-10-19 10:54:01 -0400

    Update city_state to separate attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,187 +13,184 @@ class ApplicationController < ActionController::API
 
 
         def get_results(results_array)
-           results_array.map {|result| parse_results(result)}         
-        end
-      
-        def parse_results(result)
-            id = find_id(result)
-            name = find_name(result)
-            description = find_description(result)
-            city_state = find_address(result)
-            latitude = find_latitude(result)
-            longitude = find_longitude(result)
-            url = find_url(result)
-            activities = find_activities(result)
-            images = find_images(result) 
-            find_or_create_rec_area(id, name, description, city_state, latitude, longitude, url, activities, images)
-        end
-
-        def find_or_create_rec_area(id, name, description, city_state, latitude, longitude, url, activities, images)
-                rec_area = RecArea.find_by(id)
-                rec_area.id = id.to_i
-                rec_area.name = name
-                rec_area.description = description 
-                rec_area.city_state = city_state
-                rec_area.latitude = latitude
-                rec_area.longitude = longitude
-                rec_area.url = url 
-                rec_area.activities = activities 
-                rec_area.images = images 
-                rec_area.save!
+          results_array.map {|result| parse_results(result)}         
+       end
+     
+       def parse_results(result)
+           id = find_id(result)
+          
+           name = find_name(result)
            
-             rec_area
-        end
-
-        
-
-        def find_id(result)
+           description = find_description(result)
            
-            result.each { |key, value| 
-              if key != "RecAreaID"
-              else
-                id = value 
-                return id  
-              end   
-              }      
-        end
+           city = find_city(result)
 
-        def find_name(result)
-            result.each { |key, value| 
-                if key != "RecAreaName"
-                else 
-                name = value 
-                return name  
-              end   
-              }      
-        end
+           latitude = find_latitude(result)
+           longitude = find_longitude(result)
+           url = find_url(result)
+           activities = find_activities(result)
+           images = find_images(result) 
+           find_or_create_rec_area(id, name, description, city_state, latitude, longitude, url, activities, images)
+       end
 
-        def find_description(result)
-            result.each { |key, value| 
-                if key != "RecAreaDescription"
-                else 
-                description = value
-                return description  
-              end   
-              }      
-        end
-
-    #  ADDRESS 
-
-        def find_address(result)
-            result.each { |key, value| 
-            if key != "RECAREAADDRESS" 
-            else 
-               address_items = value[0]
-               city = find_city(address_items)
-               state = find_state(address_items)
-            return "#{city}, #{state}"
+       def find_or_create_rec_area(id, name, description, city_state, latitude, longitude, url, activities, images)
+           
+           if RecArea.exists?(id)
+               rec_area = RecArea.find_by(id)
+             else
+               rec_area = RecArea.new
+               rec_area.id = id.to_i
+               rec_area.name = name
+               rec_area.description = description 
+               rec_area.city = city
+               rec_area.state = state
+               rec_area.latitude = latitude
+               rec_area.longitude = longitude
+               rec_area.url = url 
+               rec_area.activities = activities 
+               rec_area.images = images 
+               rec_area.save!
             end
-       }  
-        end
+            rec_area
+       end
 
-        def find_city(address_items)
-            address_items.each { |key, city| 
-                if key != "City"
-                else 
-                city
-                return city  
-              end   
-              }      
-        end 
+       
 
-        def find_state(address_items)
-            address_items.each { |key, state| 
-                if key != "AddressStateCode"
-                else 
-                state
-                return state  
-              end   
-              }      
-        end 
+       def find_id(result)
+          
+           result.each { |key, value| 
+             if key != "RecAreaID"
+             else
+               id = value 
+               return id  
+             end   
+             }      
+       end
 
-        # LATITUDE_LONGITUDE
-        def find_latitude(result)
-            result.each { |key, latitude| 
-            if key != "RecAreaLatitude" 
-            else 
-               latitude
-               return latitude
+       def find_name(result)
+           result.each { |key, value| 
+               if key != "RecAreaName"
+               else 
+               name = value 
+               return name  
+             end   
+             }      
+       end
+
+       def find_description(result)
+           result.each { |key, value| 
+               if key != "RecAreaDescription"
+               else 
+               description = value
+               return description  
+             end   
+             }      
+       end
+
+   #  ADDRESS 
+
+
+       def find_city(result)
+           result.each { |key, city| 
+               if key != "City"
+               else 
+               city
+               return city  
              end   
              }      
        end 
 
-       def find_longitude(result)
-        result.each { |key, longitude| 
-        if key != "RecAreaLongitude" 
-        else 
-           longitude
-           return longitude
-         end   
-         }      
-   end 
-   
+       def find_state(address_items)
+           address_items.each { |key, state| 
+               if key != "AddressStateCode"
+               else 
+               state
+               return state  
+             end   
+             }      
+       end 
+
+       # LATITUDE_LONGITUDE
+       def find_latitude(result)
+           result.each { |key, latitude| 
+           if key != "RecAreaLatitude" 
+           else 
+              latitude
+              return latitude
+            end   
+            }      
+      end 
+
+      def find_longitude(result)
+       result.each { |key, longitude| 
+       if key != "RecAreaLongitude" 
+       else 
+          longitude
+          return longitude
+        end   
+        }      
+  end 
+  
 #    URL
 
-   def find_url(result)
-            result.each { |key, value| 
-            if key != "LINK" 
-            else 
-                link = value[0] 
-                byebug
+  def find_url(result)
+           result.each { |key, value| 
+           if key != "LINK" 
+           else 
+               link = value[0] 
 
-              link.each { |link_key, url| 
-                if link_key != "URL"
-                else           
-                    url 
-                    return url   
-                end  
-            }
-            end 
-        } 
-    end 
+             link.each { |link_key, url| 
+               if link_key != "URL"
+               else           
+                   url 
+                   return url   
+               end  
+           }
+           end 
+       } 
+   end 
 
-        # ACTIVITIES
-        def find_activities(result)
-            activity_array = []
-            result.each { |key, value| 
-            if key != "ACTIVITY" 
-            else 
-               value.each {|activity| 
-               activity.each {|activity_keys, attributes| 
-               if activity_keys != "ActivityName"
+       # ACTIVITIES
+       def find_activities(result)
+           activity_array = []
+           result.each { |key, value| 
+           if key != "ACTIVITY" 
+           else 
+              value.each {|activity| 
+              activity.each {|activity_keys, attributes| 
+              if activity_keys != "ActivityName"
+              else 
+              activity_array << attributes
+            end          
+           }       
+              }        
+           end
+      }  
+       activities = activity_array.join(', ')
+       return activities
+       end
+
+       # images
+
+       def find_images(result)
+         
+         picture_array = []
+           result.each { |key, value| 
+               if key != "MEDIA"
                else 
-               activity_array << attributes
-             end          
-            }       
-               }        
-            end
-       }  
-        activities = activity_array.join(', ')
-        return activities
-        end
-
-        # images
-
-        def find_images(result)
+                   value.each {|picture|  
+                   picture.each {|picture_keys, attributes| 
+                   if picture_keys != "URL"
+                   else 
+                      
+                   picture_array << attributes
+                 end          
+                }       
+                   }      
+                end
+           }  
+            
+          picture_array.join(", ")
           
-          picture_array = []
-            result.each { |key, value| 
-                if key != "MEDIA"
-                else 
-                    value.each {|picture|  
-                    picture.each {|picture_keys, attributes| 
-                    if picture_keys != "URL"
-                    else 
-                       
-                    picture_array << attributes
-                  end          
-                 }       
-                    }      
-                 end
-            }  
-             
-           picture_array.join(", ")
-           
-             end    
+            end    
 end
