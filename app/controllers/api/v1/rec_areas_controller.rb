ca9312ccc6ca83d1b2b7e7f39f9946ce75d59f97
mrandolph83 <mrandolph83@gmail.com>
2021-10-20 18:02:29 -0400

    Reworked backend  data for final app deployment

--- app/controllers/api/v1/rec_areas_controller.rb
+++ app/controllers/api/v1/rec_areas_controller.rb
@@ -4,28 +4,35 @@ class Api::V1::RecAreasController < ApplicationController
   before_action :set_rec_area, only: [:show, :update, :destroy]
 
   def rec_area_search
-   
-    
     keywords = params[:keywords]
     state_code = params[:stateCode1]
     activity = params[:activity1]
-
-
-    search_call = RestClient.get("https://ridb.recreation.gov/api/v1/recareas?offset=0&full=true&state=#{state_code}", headers={accept: 'application/json',
-   
+    search_call = RestClient.get("https://ridb.recreation.gov/api/v1/recareas?=20&offset=0&full=true&state=#{state_code}", headers={accept: 'application/json',
     apikey: ENV["KEY"]})
-    
-    # Add a Find or Create Method here
-
     search_call_json = JSON.parse(search_call)
-    # Down to the values 
     results_array = search_call_json.values[0]
-   
     returned_results = get_results(results_array)
-
     rec_area_json = RecAreaSerializer.new(returned_results).serializable_hash.to_json
     render json: rec_area_json
 end
+
+# def rec_area_search
+#   # search_parse(params)
+#   keywords = params[:keywords] || ""
+#   state_code = params[:state_code1] || ""
+#   activity = params[:activity1] || ""
+
+#   state_parse = RecArea.where("description LIKE ?", "%" + state_code + "%").to_a
+#   activity_parse = state_parse.activities.include?("#{activity}")
+  
+# byebug  
+
+#   search_call_json = JSON.parse(search_call)
+#   results_array = search_call_json.values[0]
+#   returned_results = get_results(results_array)
+#   rec_area_json = RecAreaSerializer.new(returned_results).serializable_hash.to_json
+#   render json: rec_area_json
+# end
 
   # GET /rec_areas/1
   def show
