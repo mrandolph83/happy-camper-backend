b8581cc40ffe21c80e09910c3b9300a23dc88bab
mrandolph83 <mrandolph83@gmail.com>
2021-10-22 03:22:53 -0400

    Clean unnecessary routes

--- app/controllers/api/v1/rec_areas_controller.rb
+++ app/controllers/api/v1/rec_areas_controller.rb
@@ -3,43 +3,30 @@ class Api::V1::RecAreasController < ApplicationController
   require 'rest-client'  
   before_action :set_rec_area, only: [:show, :update, :destroy]
 
-  def rec_area_search
-    keywords = params[:keywords]
-    state_code = params[:stateCode1]
-    activity = params[:activity1]
-    search_call = RestClient.get("https://ridb.recreation.gov/api/v1/recareas?=20&offset=0&full=true&state=#{state_code}", headers={accept: 'application/json',
-    apikey: ENV["KEY"]})
-    search_call_json = JSON.parse(search_call)
-    results_array = search_call_json.values[0]
-    returned_results = get_results(results_array)
-    rec_area_json = RecAreaSerializer.new(returned_results).serializable_hash.to_json
-    render json: rec_area_json
-end
-
-# def rec_area_search
-#   # search_parse(params)
-#   keywords = params[:keywords] || ""
-#   state_code = params[:state_code1] || ""
-#   activity = params[:activity1] || ""
-
-#   state_parse = RecArea.where("description LIKE ?", "%" + state_code + "%").to_a
-#   activity_parse = state_parse.activities.include?("#{activity}")
-  
-# byebug  
-
-#   search_call_json = JSON.parse(search_call)
-#   results_array = search_call_json.values[0]
-#   returned_results = get_results(results_array)
-#   rec_area_json = RecAreaSerializer.new(returned_results).serializable_hash.to_json
-#   render json: rec_area_json
+#   def rec_area_search
+#     keywords = params[:keywords]
+#     state_code = params[:stateCode1]
+#     activity = params[:activity1]
+#     search_call = RestClient.get("https://ridb.recreation.gov/api/v1/recareas?=20&offset=0&full=true", headers={accept: 'application/json',
+#     apikey: ENV["KEY"]})
+#     search_call_json = JSON.parse(search_call)
+#     results_array = search_call_json.values[0]
+#     returned_results = get_results(results_array)
+#     # rec_area_json = RecAreaSerializer.new(returned_results).serializable_hash.to_json
+#     # render json: rec_area_json
 # end
 
-  # GET /rec_areas/1
-  def show
-    render json: @rec_area
-  end
+def rec_area_search
+  
+  keywords = params[:keywords] || ""
+  state_code = params[:stateCode1] || ""
+  activity = params[:activity1] || ""
+  full_parse = search_rec_areas(keywords, state_code, activity)
+  rec_area_json = RecAreaSerializer.new(full_parse).serializable_hash.to_json
+  # rec_area_json = state_parse.serializable_hash
 
-
+  render json: rec_area_json
+end
 
   private
     # Use callbacks to share common setup or constraints between actions.
