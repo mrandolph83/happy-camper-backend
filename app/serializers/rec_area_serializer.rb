ab062add2cbe00d78f8fd0ebbb284478cd120e3b
mrandolph83 <mrandolph83@gmail.com>
2021-10-25 11:28:37 -0400

    Full display of all review data pertained to each rec area

--- app/serializers/rec_area_serializer.rb
+++ app/serializers/rec_area_serializer.rb
@@ -2,7 +2,8 @@ class RecAreaSerializer
   include JSONAPI::Serializer
   
   set_id :id
-  attributes :name, :description, :state, :images, :latitude, :longitude, :activities
+  attributes :name, :description, :state, :images, :latitude, :longitude, :activities, :reviews
  
-  has_many :reviews, serializer: ReviewSerializer
+  # has_many :reviews, serializer: ReviewSerializer
+
 end
